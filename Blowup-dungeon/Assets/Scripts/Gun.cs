87bc915245f12e72992a4d295eb572984637c0e4
Tom Spa <tp.spa@student.avans.nl>
2022-03-07 01:00:00 +0100

    Shooting and exploding bullet

--- Blowup-dungeon/Assets/Scripts/Gun.cs
+++ Blowup-dungeon/Assets/Scripts/Gun.cs
@@ -21,29 +21,9 @@ public class Gun : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 lookDir = mousePos - rigidBody.position;
-
         float mouseAngle =  Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
 
-        Debug.Log(mouseAngle);
-
-        //if (mouseAngle < -250 || mouseAngle > 75)
-        //{
-        //    loopAngle = mouseAngle;
-        //} else
-        //{
-            loopAngle = Mathf.LerpAngle(loopAngle, mouseAngle, Time.deltaTime * 1.8f);
-        //}
-
-        //float objectAngle = transform.position * Mathf.Rad2Deg;
-
-        //Debug.Log(mouseAngle + " " + objectAngle);
-
-        ////Quaternion angle = lookDir.
-
-        //if (Mathf.DeltaAngle(objectAngle, mouseAngle) > maxRotation)
-        //{
-            
-        //}
+        loopAngle = Mathf.LerpAngle(loopAngle, mouseAngle, Time.deltaTime * 1.8f);
 
         rigidBody.transform.rotation = Quaternion.Euler(new Vector3(0, 0, loopAngle));
     }
---
+++ Blowup-dungeon/Assets/Scripts/Shooting.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform shootingPoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, shootingPoint.position, shootingPoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(shootingPoint.up * bulletForce, ForceMode2D.Impulse);
+
+
+    }
+}
